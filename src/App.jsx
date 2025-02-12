52f3028fcf3a13a774ef581db8c2f2160128661b
lavahang <llavahang@gmail.com>
2025-02-12 09:38:21 +0545

    intial

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <> 
+    <div className="search-section">
+      <form action="#" className="search-form" >
+        <span className="material-symbols-rounded">search</span>
+        <input type="search" placeholder="Enter a city name" className="search-input" required />
+      </form>
+      <button className="location-button" >
+        <span className="material-symbols-rounded">my_location</span>
+      </button>
+    </div>
+    </>
+  )
+}
+
+export default App
