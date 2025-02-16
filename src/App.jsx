ae0df36156af32aa23f0fac367a29dbf401b2d62
lavahang <llavahang@gmail.com>
2025-02-16 18:00:21 +0545

    using api matching codes for weather and made current weather

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './app.css';
 import SearchSection from './components/Searchsection';
 import CurrentWeather from './components/CurrentWeather';
 import HourlyWeatherItem from './components/HourlyWeatherItem';
+import { weatherCodes } from './constant';
 
 const App = () => {
   const [currentWeather, setCurrentWeather] = useState({});
@@ -12,12 +13,13 @@ const App = () => {
     try {
       const response = await fetch(API_URL);
       const data = await response.json();
-      console.log(data);
+      console.log(data); //can be comiited to remove display in console
 
       const temperature = Math.floor(data.current.temp_c);
       const description = data.current.condition.text;
+      const weatherIcon = Object.keys(weatherCodes).find(icon =>weatherCodes[icon].includes(data.current.condition.code))
 
-      setCurrentWeather({ temperature,description })
+      setCurrentWeather({ temperature,description, weatherIcon })
       console.log(temperature)
             
     } catch (error) {
--- src/components/CurrentWeather.jsx
+++ src/components/CurrentWeather.jsx
@@ -1,7 +1,7 @@
 const CurrentWeather = ({currentWeather}) => {
     return (
       <div className="current-weather">
-        <img src="/icons/clouds.svg" className="weather-icon" />
+        <img src={`icons/${currentWeather.weatherIcon}.svg`} className="weather-icon" />
         <h2 className="temperature">
           {currentWeather.temperature} <span>°C</span>
         </h2>
