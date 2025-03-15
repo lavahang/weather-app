ea91545c8ede12aa2d71a54ae21acb209913ea4f
lavahang <llavahang@gmail.com>
2025-03-15 21:21:13 +0545

    made correction

--- src/components/CurrentWeather.jsx
+++ src/components/CurrentWeather.jsx
@@ -3,7 +3,7 @@ const CurrentWeather = ({currentWeather}) => {
       <div className="current-weather">
         <img src={`icons/${currentWeather.weatherIcon}.svg`} className="weather-icon" />
         <h2 className="temperature">
-          {currentWeather.temperature} <span>20°C</span>
+          {currentWeather.temperature} <span>°C</span>
         </h2>
         <p className="description"> {currentWeather.description}</p>
       </div>
